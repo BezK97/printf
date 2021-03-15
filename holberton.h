5b39be8ec86dd33ad74bf4aec23bf151695bbb5f
Emanuel-js <etioaman1960@gmail.com>
2021-03-15 20:26:01 +0000

    print

--- holberton.h
+++ holberton.h
@@ -2,5 +2,13 @@
 #define HOLBERTON_H
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+int _putchar(char c);
+void format_int(int num);
+void format_un(int num);
+void format_str(char *str);
+
 #endif
