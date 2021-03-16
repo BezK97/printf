80bb4fca7cdf42613bde81fb0511c09f9a53e55c
BezK97 <bkifle97@gmail.com>
2021-03-16 08:02:25 +0000

    Print signed specifier

--- signed_sp.c
+++ signed_sp.c
@@ -4,18 +4,23 @@
  * format_int - Function that prints integer
  *
  * @num: integer to print
+ * @i: index
  *
+ * Return: i, number of printed character
  */
-void format_int(int num)
+int format_int(int num, int i)
 {
 	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
+		i++;
 	}
 	if (num / 10 != 0)
 	{
-		format_int(num / 10);
+		format_int(num / 10, i);
 	}
 	_putchar(num % 10 + '0');
+	i++;
+	return (i);
 }
