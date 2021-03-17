69b204499bced128509984dad8de76859246272c
Emanuel-js <etioaman1960@gmail.com>
2021-03-17 13:24:50 +0000

    Function Unsigned specifier

--- unsigned_sp.c
+++ unsigned_sp.c
@@ -1,27 +1,33 @@
 #include "holberton.h"
 
 /**
- * format_un - Function that prints unsigned int
+ * format_u - Function that prints unsigned integer
  *
- * @num: integer to print
- *
- * Return: i, number of character printed
+ * @arg: list of argument
+ * @buffer: array that stores string
+ * @i: pointer to number of character
  */
-int format_un(unsigned int num)
+void format_u(va_list arg, char *buffer, int *i)
 {
-	int i = 0;
-	int d = 1;
+	unsigned int x;
+	int j, numlen;
+	char *num_str;
 
-	for (; num / d > 9; )
+	x = va_arg(arg, int);
+	numlen = num_len(x);
+	num_str = malloc(numlen * sizeof(char));
+	if (num_str == NULL)
+		return;
+	utos(num_str, x);
+	for (x = *i, j = 0; j < numlen; *i += 1, x++, j++)
 	{
-		d = d * 10;
+		if (*i == 1024)
+		{
+			_putchar(buffer, i);
+			reset_putchar(buffer);
+			*i = 0;
+		}
+		buffer[*i] = num_str[j];
 	}
-	while (d > 0)
-	{
-		_putchar('0' + (num / d));
-		num = num % d;
-		d = d / 10;
-		i++;
-	}
-	return (i);
+	free(num_str);
 }
