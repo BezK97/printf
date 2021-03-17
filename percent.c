aa75a99f484bea13e16a7f9ff479b6fa5e5662b9
Emanuel-js <etioaman1960@gmail.com>
2021-03-17 13:15:45 +0000

    Function percent

---
+++ percent.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * format_pers - Function that stores percent sign in an array
+ *
+ * @arg: unused list of arguments
+ * @buffer: array to store '%'
+ * @i: pointer to the number of character
+ */
+void format_pers(__attribute__((unused))va_list arg, char *buffer, int *i)
+{
+	buffer[*i] = '%';
+	*i += 1;
+}
