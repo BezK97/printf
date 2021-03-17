930a4d74d9aa7364fbc3612c1654704680f87646
Emanuel-js <etioaman1960@gmail.com>
2021-03-17 13:18:18 +0000

    Function String and Char  Specifier

--- string_sp.c
+++ string_sp.c
@@ -3,18 +3,47 @@
 /**
  * format_str - Function that prints string
  *
- * @str: Pointer to string
- * @i: index
- *
- * Return: i, number of character printed
+ * @arg: list of argument
+ * @buffer: array that stores string
+ * @i: pointer to number of character
  */
-int format_str(char *str, int i)
+void format_str(va_list arg, char *buffer, int *i)
 {
-	if (*str == '\0')
-		return (0);
-	_putchar(*str);
-	str++;
-	format_str(str, i);
-	i++;
-	return (i);
+	int n, j;
+	char *s;
+
+	s = va_arg(arg, char*);
+
+	for (n = *i, j = 0; s[j] != '\0';  *i += 1, n++, j++)
+	{
+		if (*i == 1024)
+		{
+			_putchar(buffer, i);
+			reset_putchar(buffer);
+			*i = 0;
+		}
+		buffer[*i] = s[j];
+	}
+}
+
+/**
+ * format_c - Function that prints character
+ *
+ * @arg: list of argument
+ * @buffer: array that stores string
+ * @i: pointer to number of character
+ */
+void format_c(va_list arg, char *buffer, int *i)
+{
+	char s;
+
+	s = va_arg(arg, int);
+	if (*i == 1024)
+	{
+		_putchar(buffer, i);
+		reset_putchar(buffer);
+		*i = 0;
+	}
+	buffer[*i] = s;
+	*i += 1;
 }
