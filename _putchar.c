fe833660fd7d36e25169c00ca6058a3e10810ac6
Emanuel-js <etioaman1960@gmail.com>
2021-03-17 13:18:52 +0000

    Print Function

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,17 @@
 #include <unistd.h>
+#include "holberton.h"
+#define BUFSIZE 1024
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ *
+ * @buffer: The characters to print
+ * @i: pointer to number of character
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+void _putchar(char *buffer, int *i)
 {
-	write(1, &c, 1);
-	return (1);
+	write(1, buffer, *i);
 }
