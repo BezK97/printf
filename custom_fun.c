1bad2f2b7e02caf671ad8258a15738297e273022
Emanuel-js <etioaman1960@gmail.com>
2021-03-19 17:53:46 +0000

    Function Prototype

--- custom_fun.c
+++ custom_fun.c
@@ -55,3 +55,49 @@ void format_R(va_list arg, char *buffer, int *i)
 		}
 	}
 }
+/**
+ * format_b - Function that  convert unsigned int  to binary
+ *
+ * @arg: argument passed in
+ * @buffer: values stored
+ * @i: returns pointer to index
+ */
+void format_b(va_list arg, char *buffer, int *i)
+{
+	unsigned int n, m;
+	int *num, j, count = 0;
+	char *num_str, *num_str2;
+
+	n = va_arg(arg, int);
+	m = n;
+	if (n == 0)
+		count++;
+	for (; m != 0; count++)
+	{
+		m = m / 2;
+	}
+	num = malloc(sizeof(int) * count);
+	num_str = malloc(sizeof(char) * count);
+	num_str2 = malloc(sizeof(char) * count);
+	if (num == NULL || num_str == NULL)
+		exit(-1);
+	for (j = 0; n > 0; j++)
+	{
+		num[j] = n % 2;
+		n = n / 2;
+	}
+
+	for (j = 0; j < count; j++)
+	{
+		itos(num_str, num[j]);
+		num_str2[j] = num_str[0];
+	}
+
+	for (*i = 0, count--; count >= 0; *i += 1, count--)
+	{
+		buffer[*i] = num_str2[count];
+	}
+
+	_putchar(buffer, i);
+	*i = 0;
+}
