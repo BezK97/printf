5518417b18c2b92950c0c8854877a75666de9d26
BezK97 <bkifle97@gmail.com>
2021-03-24 15:52:46 +0000

    Function + flag

--- flag_plus.c
+++ flag_plus.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 
 /**
- * flag_plus - fuction that print + before integer if positive and - if negative
+ * flag_plus - print + before integer if positive and - if negative
  *
  * @arg: list of argument
  * @buffer: array that stores string
  * @i: pointer to number of character
+ *
+ * Return: 1 on success
  */
 int flag_plus(va_list arg, char *buffer, int *i)
 {
@@ -15,14 +17,11 @@ int flag_plus(va_list arg, char *buffer, int *i)
 	x = va_arg(arg, long int);
 
 	numlen = num_len(x);
-	numlen +=1;
+	numlen += 1;
 	num_str = malloc(numlen * sizeof(char));
 	if (num_str == NULL)
-		return(-1);
-	if (x >= 0)
-	{
-		num_str[0] = '+';
-	}
+		return (-1);
+	num_str[0] = '+';
 	itos(num_str, x);
 
 	for (x = *i, j = 0; j < numlen; *i += 1, x++, j++)
