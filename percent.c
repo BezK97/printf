93fff96b40bccb55d5ac23639f4c5b1056296b0c
BezK97 <bkifle97@gmail.com>
2021-03-25 03:02:18 +0000

    Function percent

--- percent.c
+++ percent.c
@@ -6,10 +6,12 @@
  * @arg: unused list of arguments
  * @buffer: array to store '%'
  * @i: pointer to the number of character
+ *
+ * Return: 0 on success
  */
 int format_pers(__attribute__((unused))va_list arg, char *buffer, int *i)
 {
 	buffer[*i] = '%';
 	*i += 1;
-	return(0);
+	return (0);
 }
