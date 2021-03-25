072c49c2e8c6953eb43475f7ab88ecedc9ccc354
BezK97 <bkifle97@gmail.com>
2021-03-25 02:59:09 +0000

    Function prototypes

--- holberton.h
+++ holberton.h
@@ -35,9 +35,9 @@ int format_r(va_list arg, char *buffer, int *i);
 int format_R(va_list arg, char *buffer, int *i);
 
 int flag_plus(va_list arg, char *buffer, int *i);
-int flag_spc(va_list arg, char *buffer, const char *format,int n, int *i);
-int flag_hash(va_list arg, char *buffer, const char *format,int n, int *i);
-int flag_0(va_list arg, char *buffer, const char *format,int n, int *i);
+int flag_spc(va_list arg, char *buffer, const char *format, int n, int *i);
+int flag_hash(va_list arg, char *buffer, const char *format, int n, int *i);
+int flag_0(va_list arg, char *buffer, const char *format, int n, int *i);
 
 char *itos(char str[], long int n);
 char *utos(char str[], unsigned int num);
