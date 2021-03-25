9e7f10a20cc402bfea0af8ce35257bdc3fabe278
BezK97 <bkifle97@gmail.com>
2021-03-25 11:46:31 +0000

    Function str and char specifier

--- string_sp.c
+++ string_sp.c
@@ -19,12 +19,6 @@ int format_str(va_list arg, char *buffer, int *i)
 		s = "(null)";
 	for (n = *i, j = 0; s[j] != '\0';  *i += 1, n++, j++)
 	{
-		if (*i == 1024)
-		{
-			_putchar(buffer, i);
-			reset_putchar(buffer);
-			*i = 0;
-		}
 		buffer[*i] = s[j];
 	}
 	return (0);
@@ -44,12 +38,6 @@ int format_c(va_list arg, char *buffer, int *i)
 	char s;
 
 	s = va_arg(arg, int);
-	if (*i == 1024)
-	{
-		_putchar(buffer, i);
-		reset_putchar(buffer);
-		*i = 0;
-	}
 	buffer[*i] = s;
 	*i += 1;
 	return (0);
