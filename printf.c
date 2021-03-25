9ba5cdc70211a5cc7c84b3d9075103d88f22803f
BezK97 <bkifle97@gmail.com>
2021-03-25 11:45:10 +0000

    Print function

--- printf.c
+++ printf.c
@@ -28,12 +28,12 @@ int func_sp(va_list arg, char *buffer, const char *format, int n, int *i)
 	for (j = 0; specifier[j].sp != '\0'; j++)
 	{
 		if (specifier[j + 1].sp == '\0' && specifier[j].sp != '0')
-                {
-                        buffer[*i] = format[n - 1];
-                        *i += 1;
-                        buffer[*i] = format[n];
-                        *i += 1;
-                }
+		{
+			buffer[*i] = format[n - 1];
+			*i += 1;
+			buffer[*i] = format[n];
+			*i += 1;
+		}
 		if (format[n] == specifier[j].sp)
 		{
 			if ((format[n] == ' ' || format[n] == '#') ||
@@ -56,24 +56,18 @@ int func_sp(va_list arg, char *buffer, const char *format, int n, int *i)
 
 int _printf(const char *format, ...)
 {
-	char buffer[1024];
+	char *buffer = malloc(2048);
 	int n, x = 0, *i = &x;
 
 	va_list arg;
 
 	if (!format)
-		return (-1);
+		return (free(buffer), -1);
 	va_start(arg, format);
 	for (n = 0; format[n] != '\0'; n++)
 	{
 		for (; format[n] != '%' && format[n] != '\0'; *i += 1, n++)
 		{
-			if (*i == 1024)
-			{
-				_putchar(buffer, i);
-				reset_putchar(buffer);
-				*i = 0;
-			}
 			buffer[*i] = format[n];
 		}
 		if (format[n] == '\0')
@@ -86,8 +80,7 @@ int _printf(const char *format, ...)
 			n += func_sp(arg, buffer, format, n, i);
 		}
 	}
-	va_end(arg);
 	buffer[*i] = '\0';
 	_putchar(buffer, i);
-	return (*i);
+	return (va_end(arg), free(buffer), *i);
 }
