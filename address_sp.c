8a54f1b4fb3341f30cbfc8b08550c14357899a76
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-26 16:48:38 +0300

    Update address_sp.c

--- address_sp.c
+++ address_sp.c
@@ -4,7 +4,7 @@
  * format_p - Function that prints address.
  *
  * @arg: list of argument.
- * @buffer: array that stores string
+ * @buffer: array that stores string.
  * @i: pointer to number of character
  *
  * Return: 1 on success.
