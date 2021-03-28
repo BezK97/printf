0b307b5ebd8cb58e632cc8045674dcd6e574c633
BezK97 <bkifle97@gmail.com>
2021-03-28 17:17:48 +0000

    Custom function

--- custom_fun.c
+++ custom_fun.c
@@ -55,3 +55,55 @@ int format_R(va_list arg, char *buffer, int *i)
 	}
 	return (0);
 }
+
+/**
+ * format_S - functtion that prints hex values of  0<ASCII value<32 or >=127
+ *
+ * @arg: argument passed
+ * @buffer: values stored
+ * @i: tracks index position
+ *
+ * Return: 0 on success
+ */
+int format_S(va_list arg, char *buffer, int *i)
+{
+	char *str, *hexvalues, *hex;
+	int k, l, len = 0, m;
+
+	str = va_arg(arg, char *);
+	hexvalues = "0123456789ABCDEF";
+	hex = malloc(2 * sizeof(char));
+
+	for (k = 0; str[k] != 0; k++, *i += 1)
+	{
+		if ((str[k] > 0 && str[k] < 32) || str[k] >= 127)
+		{
+			m = str[k];
+			for (l = 0, len = 0; m != 0; l++, len++)
+			{
+				hex[l] = hexvalues[m % 16];
+				m /= 16;
+			}
+			buffer[*i] = '\\';
+			*i += 1;
+			buffer[*i] = 'x';
+			*i += 1;
+			if (len == 1)
+			{
+				buffer[*i] = '0';
+				*i += 1;
+				buffer[*i] = hex[len - 1];
+			}
+			else
+			{
+				buffer[*i] = hex[len - 1];
+				*i += 1;
+				buffer[*i] = hex[len - 2];
+			}
+		}
+		else
+			buffer[*i] = str[k];
+	}
+	free(hex);
+	return (0);
+}
