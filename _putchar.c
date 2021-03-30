1dad91ce77f4f0a12e01838a7e572926382a058b
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-30 18:18:51 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout.
+ * _putchar - writes the character c to stdout
  *
  * @buffer: The characters to print.
  * @i: pointer to number of character
